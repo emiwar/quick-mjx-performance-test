2e9b5a63d0f07f34edaed4cf3977ca34a5d09475
Emil Warnberg <ewarnberg@g.harvard.edu>
2024-03-01 10:26:21 -0500

    Basic benchmarks for ant, humanoid and virtual rat.

--- mjx_naive.py
+++ mjx_naive.py
@@ -1,12 +1,17 @@
 import time
+import etils
 
 import jax
 import mujoco
 from mujoco import mjx
 import tqdm
+import pandas as pd
 
 print("Loading model")
-model = mujoco.MjModel.from_xml_path('rodent.xml')
+#model = mujoco.MjModel.from_xml_path('ant.xml')
+path = etils.epath.Path(etils.epath.resource_path('mujoco')) / ('mjx/test_data/humanoid')
+model = mujoco.MjModel.from_xml_path((path / 'humanoid.xml').as_posix())
+
 mjx_model = mjx.device_put(model)
 
 @jax.vmap
@@ -35,13 +40,13 @@ def time_mjx(batch_size, n_sim_steps=100):
 
 if __name__ == '__main__':
     results = []
-    for batch_size in (1,2,4,8,16,32,64,128,256,512):
+    for batch_size in (1,2,4,8,16,32,64,128,256,512,1024,2048,4096,4096*2):
         results.append(time_mjx(batch_size))
     results_df = pd.DataFrame(results, columns=["batch_size", "wallclock_time", "steps_per_second"])
     results_df['simulator'] = 'mjx'
-    results_df['parallellisation'] = 'multiprocessing'
-    results_df['model'] = 'virtual_rat_mjx_version'
+    results_df['parallellisation'] = 'none'
+    results_df['model'] = 'humanoid_mjx'
     results_df['parition'] = 'gpu_test'
     results_df['control_inputs'] = 'none'
     results_df['rendering'] = 'none'
-    results_df.to_csv('results/mjx_rat.csv')
+    results_df.to_csv('results/mjx_humanoid_mjx.csv')
