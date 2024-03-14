0ed0c325624e08fce31610bea594d8ecbb26c37d
Emil Warnberg <ewarnberg@g.harvard.edu>
2024-03-14 17:36:11 -0400

    Some minor optimizations to the benchmarking.

--- test_mujuco_rollouts.py
+++ test_mujuco_rollouts.py
@@ -11,8 +11,8 @@ model = mujoco.MjModel.from_xml_path(model_filename)
 data = mujoco.MjData(model)
 state_size = mujoco.mj_stateSize(model, mujoco.mjtState.mjSTATE_FULLPHYSICS)
 
-n_steps_list = np.round(2**np.arange(0, 11.25, 0.25)).astype('int')
-n_envs_list = np.round(2**np.arange(0, 11.25, 0.25)).astype('int')
+n_steps_list = np.round(2**np.arange(0, 9.25, 0.25)).astype('int')
+n_envs_list = np.round(2**np.arange(0, 9.25, 0.25)).astype('int')
 
 test_combinations = itertools.product(n_steps_list, n_envs_list)
 n_test_combinations = len(n_steps_list)*len(n_envs_list)
@@ -20,14 +20,24 @@ results = []
 
 for n_steps, n_envs in tqdm.tqdm(test_combinations, total=n_test_combinations):
     mujoco.mj_resetData(model, data)
+    #It seems it might be necessary to do a single-step rollout first
+    #after resetting to get consistent results?
+    mujoco.rollout.rollout(model, data,
+                           np.zeros((n_envs,state_size)),
+                           np.zeros((n_envs, 1, model.nu)))
+    
     initial_state = np.zeros((n_envs, state_size))
     control = np.zeros((n_envs, n_steps, model.nu))
+    #Pre-allocate outputs
+    state = np.empty((n_envs, n_steps, state_size))
+    sensordata = np.empty((n_envs, n_steps, model.nsensordata))
     start_time = time.time()
-    state, sensordata = mujoco.rollout.rollout(model, data, initial_state, control)
+    mujoco.rollout.rollout(model, data, initial_state, control,
+                           state=state, sensordata=sensordata)
     end_time = time.time()
     results.append((model_filename, n_steps, n_envs, end_time - start_time))
 
 results_df = pd.DataFrame(results, columns=["model", "n_steps",
                                             "n_envs", "run_time"])
 results_df["steps_per_second"] = results_df.eval("n_steps * n_envs / run_time")
-results_df.to_csv(f"basic_mujoco_rollout_humanoid.csv")
+results_df.to_csv(f"results/basic_mujoco_rollout_humanoid.csv")
