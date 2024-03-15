9d29d8ad0bf357193afe54830626117f7400ffb8
Emil Warnberg <ewarnberg@g.harvard.edu>
2024-03-15 13:38:23 -0400

    Tested with using warmstarts, but seems much worse?

--- test_mujuco_rollouts.py
+++ test_mujuco_rollouts.py
@@ -6,7 +6,7 @@ import pandas as pd
 import itertools
 import time
 
-model_filename = 'rodent.xml'
+model_filename = 'humanoid.xml'
 model = mujoco.MjModel.from_xml_path(model_filename)
 data = mujoco.MjData(model)
 state_size = mujoco.mj_stateSize(model, mujoco.mjtState.mjSTATE_FULLPHYSICS)
@@ -20,11 +20,12 @@ results = []
 
 for n_steps, n_envs in tqdm.tqdm(test_combinations, total=n_test_combinations):
     mujoco.mj_resetData(model, data)
+    initial_warmstart = data.qacc_warmstart.copy()
     #It seems it might be necessary to do a single-step rollout first
     #after resetting to get consistent results?
-    mujoco.rollout.rollout(model, data,
-                           np.zeros((n_envs,state_size)),
-                           np.zeros((n_envs, 1, model.nu)))
+    #mujoco.rollout.rollout(model, data,
+    #                       np.zeros((n_envs,state_size)),
+    #                       np.zeros((n_envs, 1, model.nu)))
     
     initial_state = np.zeros((n_envs, state_size))
     control = np.zeros((n_envs, n_steps, model.nu))
@@ -33,11 +34,12 @@ for n_steps, n_envs in tqdm.tqdm(test_combinations, total=n_test_combinations):
     sensordata = np.empty((n_envs, n_steps, model.nsensordata))
     start_time = time.time()
     mujoco.rollout.rollout(model, data, initial_state, control,
-                           state=state, sensordata=sensordata)
+                           state=state, sensordata=sensordata,
+                           initial_warmstart=initial_warmstart)
     end_time = time.time()
     results.append((model_filename, n_steps, n_envs, end_time - start_time))
 
 results_df = pd.DataFrame(results, columns=["model", "n_steps",
                                             "n_envs", "run_time"])
 results_df["steps_per_second"] = results_df.eval("n_steps * n_envs / run_time")
-results_df.to_csv(f"results/basic_mujoco_rollout_rodent.csv")
+results_df.to_csv(f"results/basic_mujoco_rollout_humanoid_warmstart.csv")
