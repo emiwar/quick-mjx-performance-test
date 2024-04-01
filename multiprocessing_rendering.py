9c1d9c72f468bfe516205b99bc50fcd41d8199db
Emil Warnberg <ewarnberg@g.harvard.edu>
2024-04-01 14:04:54 -0400

    Started benchmarking rendering.

--- multiprocessing_rendering.py
+++ multiprocessing_rendering.py
@@ -13,22 +13,23 @@ def process_initializer():
     global model
     global data
     global renderer
-    global ctx
+    #global ctx
     data = mujoco.MjData(model)
-    ctx = mujoco.GLContext(64, 64)
-    ctx.make_current()
+    #ctx = mujoco.GLContext(64, 64)
+    #ctx.make_current()
     renderer = mujoco.Renderer(model, height=64, width=64)
     
 def call_render(_):
     global data
     global model
-    for i in range(1):
+    for i in range(20):
         renderer.update_scene(data)
         renderer.render()
 
 if __name__ == '__main__':
-    n_workers = 1
+    multiprocessing.set_start_method('spawn')
+    n_workers = 20
     with multiprocessing.Pool(n_workers, process_initializer) as p:
-        for t in tqdm.trange(1):
+        for t in tqdm.trange(100):
             res = p.map(call_render, np.arange(n_workers))
         
---
+++ rendering/benchmark_rendering_settings.py
@@ -0,0 +1,78 @@
+import os
+import mujoco
+import mujoco.rollout
+import tqdm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+import time
+
+def benchmark_rendering(model_filename, camera, res, steps, backend="egl",
+                        visJoint=False, visSkin=False,
+                        visFlexSkin=False, visTexture=True,
+                        visFog=False, visShadow=False, visReflection=False):
+        os.environ['MUJOCO_GL'] = backend
+        model = mujoco.MjModel.from_xml_path(model_filename)
+        scene_option = mujoco.MjvOption()
+        scene_option.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = visJoint
+        scene_option.flags[mujoco.mjtVisFlag.mjVIS_SKIN] = visSkin
+        scene_option.flags[mujoco.mjtVisFlag.mjVIS_FLEXSKIN] = visFlexSkin
+        scene_option.flags[mujoco.mjtVisFlag.mjVIS_TEXTURE] = visTexture
+        model.vis.global_.offheight = res
+        model.vis.global_.offwidth = res
+        data = mujoco.MjData(model)
+        with mujoco.Renderer(model, height=res, width=res) as rend:
+            mujoco.mj_step(model, data)
+            #mujoco.mj_forward(model, data)
+            img = rend.render()
+            s_time = time.time()
+            for i in range(steps):
+                rend.update_scene(data, camera=camera, scene_option=scene_option)
+                rend.scene.flags[mujoco.mjtRndFlag.mjRND_FOG] = visFog
+                rend.scene.flags[mujoco.mjtRndFlag.mjRND_SHADOW] = visShadow
+                rend.scene.flags[mujoco.mjtRndFlag.mjRND_REFLECTION] = visReflection
+                rend.render(out=img)
+            e_time = time.time()
+        return steps / (e_time - s_time)
+
+model_filename = '../rodent.xml'
+
+setups = [
+    dict(model_filename=model_filename, camera="side", res=256, steps=1024*8, backend="egl"),
+    dict(model_filename=model_filename, camera="egocentric", res=256, steps=1024*8, backend="egl"),
+    dict(model_filename=model_filename, camera="egocentric", res=64, steps=1024*8, backend="egl"),
+]
+
+features = [
+    dict(visJoint=True,  visSkin=True, visFlexSkin=True, visTexture=True, visFog=True, visShadow=True, visReflection=True),
+    dict(visJoint=False, visSkin=True, visFlexSkin=True, visTexture=True, visFog=True, visShadow=True, visReflection=True),
+    dict(visJoint=False, visSkin=True, visFlexSkin=False, visTexture=True, visFog=False, visShadow=True, visReflection=True),
+    dict(visJoint=False, visSkin=False, visFlexSkin=False, visTexture=True, visFog=False, visShadow=True, visReflection=True),
+    dict(visJoint=False,  visSkin=False, visFlexSkin=False, visTexture=True, visFog=True, visShadow=True, visReflection=True),
+    dict(visJoint=False, visSkin=False, visFlexSkin=False, visTexture=True, visFog=False, visShadow=False, visReflection=True),
+    dict(visJoint=False, visSkin=False, visFlexSkin=False, visTexture=True, visFog=False, visShadow=True, visReflection=False),
+    dict(visJoint=False, visSkin=False, visFlexSkin=False, visTexture=True, visFog=False, visShadow=False, visReflection=False),
+    dict(visJoint=False, visSkin=False, visFlexSkin=False, visTexture=False, visFog=True, visShadow=True, visReflection=True),
+    dict(visJoint=False, visSkin=True, visFlexSkin=False, visTexture=True, visFog=False, visShadow=False, visReflection=False),
+    dict(visJoint=False, visSkin=True, visFlexSkin=False, visTexture=False, visFog=False, visShadow=False, visReflection=False),
+    dict(visJoint=False, visSkin=True, visFlexSkin=True, visTexture=True, visFog=False, visShadow=False, visReflection=False),
+    dict(visJoint=False, visSkin=True, visFlexSkin=True, visTexture=False, visFog=False, visShadow=False, visReflection=False),
+    dict(visJoint=False, visSkin=True, visFlexSkin=True, visTexture=False, visFog=True, visShadow=False, visReflection=False),
+    dict(visJoint=False, visSkin=False, visFlexSkin=False, visTexture=False, visFog=True, visShadow=False, visReflection=False),
+    dict(visJoint=True, visSkin=False, visFlexSkin=False, visTexture=False, visFog=False, visShadow=False, visReflection=False),
+    dict(visJoint=False, visSkin=False, visFlexSkin=False, visTexture=False, visFog=False, visShadow=False, visReflection=False),
+]
+
+results = []
+for setup in setups:
+    camera = setup["camera"]
+    res = setup["res"]
+    for feature_set in tqdm.tqdm(features, desc=f"{camera}, {res}x{res}"):
+        setup.update(feature_set)
+        result = setup.copy()
+        result["fps"] = benchmark_rendering(**setup)
+        results.append(result)
+pd.DataFrame(results).to_csv("results/feature_benchmarking_local.csv")
+
